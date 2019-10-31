ca68ad17ab1f0ff696c496dd3285d817fa7d7c24
Peh Xian Bin, Eugene <eugenepeh@users.noreply.github.com>
2019-10-31 20:29:06 +0800

    Refactor script to api (#7)

--- delete_message.py
+++ delete_message.py
@@ -1,66 +1,97 @@
 import time
-
 import requests
 
-# Using a separate file to hold the configurable fields.
-from config import *
 
-s = requests.Session()
-s.headers = {'authorization': AUTHORIZATION}
-offset = 0
-current_timestamp = ""
-while True:
-    # the url for deleting direct message and channel message is different
-    if IS_DIRECT_MESSAGE:
-        url = "https://discordapp.com/api/v6/channels/%s/messages/search?author_id=%s&attempts=1&offset=%s"
-    else:
-        url = "https://discordapp.com/api/v6/guilds/%s/messages/search?author_id=%s&include_nsfw=true&offset=%s"
+def delete_direct_messages(auth, author_id, server_id, del_pinned):
+    """
+    :param auth: is a long string about 88 characters which can be found by inspecting network to discord
+            In the `Request Headers` -> `authorization`.
 
-    full_url = url % (TARGET_SERVER_ID, TARGET_AUTHOR_ID, str(offset))
-    print(full_url)
-    p = s.get(full_url)
+    :param author_id:
+    :param server_id:
+            author_id and server_id can be easily copied if you enable the developer mode on discord.
+            To do that, go to `Settings` -> `Appearance` -> `Advanced` -> Enable `Developer Mode`.
+            You should now be able to right click on server and user icons and select `Copy ID`.
 
-    if p.status_code is not 200:
-        print("Error %d has occurred during get." % p.status_code)
-        print(p.content)
-        exit(0)
+    :param del_pinned: Set it to True if you wish to delete the pinned messages as well.
+    """
+    delete_messages(auth, author_id, server_id, True, del_pinned)
 
-    # no more messages to delete
-    if len(p.json()["messages"]) is 0:
-        print("No messages to delete!")
-        break
 
-    acceptable_codes = [200, 204, 404]
-    for messages in p.json()["messages"]:
-        for message in messages:
-            message_id = message["id"]
-            message_timestamp = message["timestamp"]
-            current_timestamp = message_timestamp if current_timestamp == "" else current_timestamp
-            pinned = message["pinned"]
+def delete_messages(auth, author_id, server_id, dm=False, del_pinned=False):
+    """
+    :param auth: is a long string about 88 characters which can be found by inspecting network to discord
+            In the `Request Headers` -> `authorization`.
 
-            # Ignore all the messages not authored by the target author
-            if "hit" not in message or not message["hit"]:
-                continue
+    :param author_id:
+    :param server_id:
+            author_id and server_id can be easily copied if you enable the developer mode on discord.
+            To do that, go to `Settings` -> `Appearance` -> `Advanced` -> Enable `Developer Mode`.
+            You should now be able to right click on server and user icons and select `Copy ID`.
 
-            # This helps to keep your conversation going while the script deletes old messages
-            if message_timestamp > current_timestamp:
-                offset += 1
-                continue
+    :param dm: Set it to True if the target server is a direct message channel.
+    :param del_pinned: Set it to True if you wish to delete the pinned messages as well.
+    """
+    s = requests.Session()
+    s.headers = {'authorization': auth}
+    offset = 0
+    current_timestamp = ""
+    while True:
+        # the url for deleting direct message and channel message is different
+        if dm:
+            url = "https://discordapp.com/api/v6/channels/%s/messages/search?author_id=%s&attempts=1&offset=%s"
+        else:
+            url = "https://discordapp.com/api/v6/guilds/%s/messages/search?author_id=%s&include_nsfw=true&offset=%s"
 
-            if pinned and TO_DELETE_PINNED:
-                print(message)
-                print("PINNED!")
-                offset += 1
-                continue
+        full_url = url % (server_id, author_id, str(offset))
+        print(full_url)
+        p = s.get(full_url)
 
-            if message["author"]["id"] == TARGET_AUTHOR_ID:
-                d = s.delete("https://discordapp.com/api/v6/channels/%s/messages/%s"
-                             % (message["channel_id"], message["id"]))
-                if d.status_code in acceptable_codes:
-                    print("Deleted message: %s" % message)
-                else:
-                    print("Error %d has occurred during delete." % d.status_code)
-                    print(d.content)
-                    exit(0)
+        if p.status_code is not 200:
+            print("Delete Messages: error %d has occurred during get." % p.status_code)
+            print(p.content)
+            return
 
-                time.sleep(0.1)
+        # no more messages to delete
+        if len(p.json()["messages"]) is 0:
+            print("No messages to delete!")
+            break
+
+        acceptable_codes = [200, 204, 404]
+        for messages in p.json()["messages"]:
+            for message in messages:
+                message_timestamp = message["timestamp"]
+                current_timestamp = message_timestamp if current_timestamp == "" else current_timestamp
+                pinned = message["pinned"]
+
+                # Ignore all the messages not authored by the target author
+                if "hit" not in message or not message["hit"]:
+                    continue
+
+                # This helps to keep your conversation going while the script deletes old messages
+                if message_timestamp > current_timestamp:
+                    offset += 1
+                    continue
+
+                if pinned and del_pinned:
+                    print("Deleted pinned message: " + message)
+                    offset += 1
+                    continue
+
+                if message["author"]["id"] == author_id:
+                    d = s.delete("https://discordapp.com/api/v6/channels/%s/messages/%s"
+                                 % (message["channel_id"], message["id"]))
+                    if d.status_code in acceptable_codes:
+                        print("Deleted message: %s" % message)
+                    else:
+                        print("Delete Messages: error %d has occurred during delete." % d.status_code)
+                        print(d.content)
+                        return
+
+                    time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    # Using a separate file to hold the configurable fields.
+    from config import *
+    delete_messages(AUTHORIZATION, TARGET_AUTHOR_ID, TARGET_SERVER_ID, IS_DIRECT_MESSAGE, TO_DELETE_PINNED)
